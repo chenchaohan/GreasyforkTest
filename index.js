7459fd9adeb24b18330bdb7f88e43757df96d685
superhan <chenchaohan@live.com>
2022-08-13 10:48:35 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Test
+// @namespace    https://beihuolang.net
+// @version      0.1
+// @description  try to take over the world!
+// @author       superhan
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("hello superhan ")
+    // Your code here...
+})();
